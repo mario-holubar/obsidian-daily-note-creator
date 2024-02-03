5cfa1f423d1c647b3605c1db7f867d2baad1dc74
Mario Holubar <34423543+mario-holubar@users.noreply.github.com>
2024-02-03 23:01:51 +0100

    get date bounds

--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { appHasDailyNotesPluginLoaded, getDailyNoteSettings, getAllDailyNotes, getDailyNote, createDailyNote } from "obsidian-daily-notes-interface";
+import { moment } from 'obsidian';
 
 interface DailyNoteCreatorSettings {
 }
 
 const DEFAULT_SETTINGS: DailyNoteCreatorSettings = {
+}
+
+// Find the date of the first and last daily notes that exist in the vault
+function getFirstAndLastDates(dailyNotes: Record<string, TFile>) {
+	const sortedDailyNotes = Object.entries(dailyNotes).sort();
+
+	// Fall back to today's date if there are no daily notes
+	if (sortedDailyNotes.length === 0) {
+		return { first: moment(), last: moment() };
+	}
+
+	let { format } = getDailyNoteSettings();
+	const [, firstNote] = sortedDailyNotes[0];
+	const [, lastNote] = sortedDailyNotes[sortedDailyNotes.length - 1];
+	const firstDate = moment(firstNote.basename, format);
+	const lastDate = moment(lastNote.basename, format);
+
+	return { first: firstDate, last: lastDate };
 }
 
 export default class DailyNoteCreator extends Plugin {
@@ -15,6 +35,11 @@ export default class DailyNoteCreator extends Plugin {
 
 		// Create settings tab
 		this.addSettingTab(new DailyNoteCreatorSettingTab(this.app, this));
+		
+		this.app.workspace.onLayoutReady(async () => {
+			const dailyNotes = await getAllDailyNotes();
+			console.log(getFirstAndLastDates(dailyNotes));
+		});
 	}
 
 	onunload() {
